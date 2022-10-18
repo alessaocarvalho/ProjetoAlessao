85af9a00b5ac987b6b3f48e56095e11d97619057
Alisson Carvalho <alessaocarvalho@outlook.com>
2022-10-18 17:38:02 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAlessao/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoAlessao.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+
+        }
+    }
+}
---
+++ ProjetoAlessao/Models/Veiculo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjetoAlessao.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o Nome!")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Obrigatório informar a Placa!")]
+        public string Placa { get; set; }
+    }
+}
